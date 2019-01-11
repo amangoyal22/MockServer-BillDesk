e8549bdca1f28ef95a3769fd27ceb6cf8cec0b49
amangoyal22 <aman.goyalwd@gmail.com>
2019-01-11 11:24:48 +0530

    initial commit

---
+++ src/main/java/com/example/billdesk/BilldeskApplication.java
@@ -0,0 +1,14 @@
+package com.example.billdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilldeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilldeskApplication.class, args);
+    }
+
+}
+
